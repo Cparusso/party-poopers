73b86c289d6ff99457bd3f91f119f3f7d2e45fa4
Charlie Russo <cparusso@gmail.com>
2019-02-14 07:48:28 -0500

    Add notes to the direction card component regarding the previous commit.

--- src/components/DirectionCard.js
+++ src/components/DirectionCard.js
@@ -14,3 +14,9 @@ class DirectionCard extends Component {
 }
 
 export default DirectionCard
+
+// Option:
+// Add the below onclick to the direction card (in the p tag)
+// *** onClick={ () => this.props.changeAction() ***
+// This addition, in conjunction to a counter, would allow the user to manually skip the currently selected
+// action if needed - a sort of handicap for the single player experience
